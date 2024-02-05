d3227d4ce2dcda28c8c6393be608b9ded55197c3
s3bxst1 <s3bxs@outlook.com>
2024-02-05 14:48:45 -0500

    Initial commit

---
+++ src/Filter.java
@@ -0,0 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+public class interface
---
+++ src/ShortLister.java
@@ -0,0 +1,55 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+
+// ShortLister program
+public class ShortLister {
+    public static void main(String[] args) {
+        JFileChooser fileChooser = new JFileChooser();
+        int returnValue = fileChooser.showOpenDialog(null);
+
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            java.io.File selectedFile = fileChooser.getSelectedFile();
+            List<String> shortWords = collectShortWords(selectedFile);
+
+            // Printing short words from the file
+            System.out.println("Short words from the file:");
+            for (String word : shortWords) {
+                System.out.println(word);
+            }
+        }
+    }
+
+    // Method to collect short words from the file
+    public static List<String> collectShortWords(java.io.File file) {
+        List<String> shortWords = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] words = line.split("\\s+");
+                for (String word : words) {
+                    if (word.length() < 5) {
+                        shortWords.add(word);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return shortWords;
+    }
+}
+// ShortWordFilter class implementing Filter interface
+class ShortWordFilter implements Filter<String> {
+    @Override
+    public boolean accept(String word) {
+        return word.length() < 5;
+    }
+}
